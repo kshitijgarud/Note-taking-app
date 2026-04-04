3fcd5d4a84bd36a824e5b4f6eb8bda2ad78af754
Kshitij Garud <kshitijgarud00@gmail.com>
2026-04-04 20:21:11 +0530

    added ratelimiter

--- server/src/server.js
+++ server/src/server.js
@@ -2,23 +2,26 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middlewares/rateLimiter.js";
 
 const port = process.env.PORT || 3000;
 dotenv.config()
 const app = express();
-connectDB();
+
 
 // Middleware 
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
-
+app.use(rateLimiter);
 // our simple custom middleware
-app.use((req,res,next)=>{
-    console.log(`Req method is ${req.method} & URL is ${req.url}`);
-    next();
-})
+// app.use((req,res,next)=>{
+//     console.log(`Req method is ${req.method} & URL is ${req.url}`);
+//     next();
+// });
 
 app.use("/api/notes",notesRoutes);
 
-app.listen(port,()=>{
+connectDB().then(()=>{
+    app.listen(port,()=>{
     console.log("listening to port ",port);
-})
+    });
+});
